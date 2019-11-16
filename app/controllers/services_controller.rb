7dc2bfb956a1e551637d4309d049afa61f32ad7d
chalpenny <cp.halpenny@gmailcom>
2019-11-16 11:34:55 -0800

    Fix Add Services form

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,13 +1,13 @@
 class ServicesController < ApplicationController
 
     get "/services/new" do
+        redirect_if_not_logged_in
         erb :"/services/new.html"
     end
 
     post "/services" do
-        binding.pry
-        @service = Service.create(service_item: params[:service_item],date: params[:date],mechanic: params[:mechanic], vehicle_id: params[:vehicle_id])
-        binding.pry
+        redirect_if_not_logged_in
+        @service = Service.create(service_item: params[:service_item],date: params[:date],mechanic: params[:mechanic], cost: params[:cost],vehicle_id: params[:vehicle_id])
         redirect "/vehicles/#{@service[:vehicle_id]}"
     end
 
