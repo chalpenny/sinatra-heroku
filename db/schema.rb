8fbab8c93e5a2d5c0396e1391405ad12872af6bd
chalpenny <cp.halpenny@gmailcom>
2019-11-16 22:44:30 -0800

    3rd attempt to fix commit issue

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191111052801) do
+ActiveRecord::Schema.define(version: 20191117064232) do
 
   create_table "services", force: :cascade do |t|
     t.string  "service_item"
