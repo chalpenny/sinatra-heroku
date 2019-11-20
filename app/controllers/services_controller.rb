fce458e487d6ce25a185f182bb2d6c763f4fae65
chalpenny <cp.halpenny@gmailcom>
2019-11-19 22:49:24 -0800

    Remove binding.pry and unused route and form

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,10 +1,5 @@
 class ServicesController < ApplicationController
 
-    get "/services/new" do
-        redirect_if_not_logged_in
-        erb :"/services/new.html"
-    end
-
     post "/services" do
         redirect_if_not_logged_in
         @service = Service.create(service_item: params[:service_item],date: params[:date],mechanic: params[:mechanic], cost: params[:cost],vehicle_id: params[:vehicle_id])
